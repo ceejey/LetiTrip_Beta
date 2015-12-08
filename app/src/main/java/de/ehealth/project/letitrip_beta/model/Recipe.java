a5f0efb8cbf94288fe204d09bbeaa3f983b7ff77
max <justno@no.com>
2015-12-08 15:53:18 +0100

    cleanup

--- app/src/main/java/de/ehealth/project/letitrip_beta/model/Recipe.java
+++ app/src/main/java/de/ehealth/project/letitrip_beta/model/Recipe.java
@@ -4,13 +4,5 @@ package de.ehealth.project.letitrip_beta.model;
  * Created by eHealth on 08.12.2015.
  */
 public class Recipe {
-
-    public Recipe(){
-<<<<<<< Updated upstream
-        Integer test = 1;
-=======
-        Integer test = 2;
->>>>>>> Stashed changes
-        Integer test = 4;
-    }
+    
 }

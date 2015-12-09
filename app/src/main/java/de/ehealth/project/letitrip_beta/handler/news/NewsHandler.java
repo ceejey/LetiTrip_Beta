2ba1b089cbee79e5afe0616fc0f2a5eb69efee93
max <justno@no.com>
2015-12-09 23:09:24 +0100

    Dashboard News fast fertig

---
+++ app/src/main/java/de/ehealth/project/letitrip_beta/handler/news/NewsHandler.java
@@ -0,0 +1,151 @@
+package de.ehealth.project.letitrip_beta.handler.news;
+
+import android.app.Activity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import de.ehealth.project.letitrip_beta.R;
+import de.ehealth.project.letitrip_beta.handler.task.news.DownloadImageTask;
+import de.ehealth.project.letitrip_beta.handler.task.news.NewsTask;
+import de.ehealth.project.letitrip_beta.model.news.Content;
+import de.ehealth.project.letitrip_beta.model.news.Image;
+import de.ehealth.project.letitrip_beta.model.news.News;
+import de.ehealth.project.letitrip_beta.model.news.Story;
+import de.ehealth.project.letitrip_beta.view.MainActivity;
+import de.ehealth.project.letitrip_beta.view.fragment.FragmentChanger;
+
+/**
+ * Created by eHealth on 09.12.2015.
+ */
+public class NewsHandler {
+
+    private static Story mSelectedStory;
+
+    public static void fillNewsFeed(final View view, final LayoutInflater inflater, final Activity activity) {
+
+        try {
+
+            final String newsJson = new NewsTask("gesundheit", "dbce43690d443111a49fc0b38f48e2d4", "de", 15).execute().get();
+
+            if (!newsJson.isEmpty()) {
+
+                activity.runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+
+                        Gson gson = new Gson();
+                        News news = gson.fromJson(newsJson, News.class);
+
+                        Content content = news.getContent();
+                        List<Story> storyList = content.getStory();
+
+                        for (Story story : storyList) {
+
+                            LinearLayout placeHolder = new LinearLayout(view.findViewById(R.id.layoutDashboard).getContext());
+                            inflater.inflate(R.layout.news_view, placeHolder);
+                            ((LinearLayout) view.findViewById(R.id.layoutDashboard)).addView(placeHolder);
+
+                            TextView txtNewsImgCaption = (TextView) placeHolder.findViewById(R.id.txtNewsImgCaption);
+                            ImageView imgNews = (ImageView) placeHolder.findViewById(R.id.imgNews);
+
+                            if (story.getMedia() != null) {
+
+                                List<Image> imageList = story.getMedia().getImage();
+
+                                for (Image image : imageList) {
+
+                                    new DownloadImageTask(imgNews).execute(image.getUrl());
+
+                                    String caption = image.getCaption();
+                                    caption = caption.substring(caption.indexOf("\"") + 1, caption.lastIndexOf("\""));
+                                    txtNewsImgCaption.setText(caption);
+
+                                }
+
+                            } else {
+                                txtNewsImgCaption.setVisibility(View.GONE);
+                                imgNews.setVisibility(View.GONE);
+                            }
+
+
+                            TextView txtNewsSubHeading = (TextView) placeHolder.findViewById(R.id.txtNewsSubheading);
+                            TextView txtNewsBody = (TextView) placeHolder.findViewById(R.id.txtNewsBody);
+
+                            txtNewsSubHeading.setText(story.getTitle());
+                            String published = story.getPublished();
+                            txtNewsBody.setText(story.getRessort() + " vom: " + published.substring(0, published.indexOf("T")));
+
+                            final Story clickedStory = story;
+
+                            placeHolder.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+
+                                    if (activity instanceof FragmentChanger) {
+
+                                        mSelectedStory = clickedStory;
+
+                                        FragmentChanger changer = (FragmentChanger) activity;
+                                        changer.changeFragment(MainActivity.FragmentName.NEWS);
+
+                                    } else {
+                                        Log.e("Error", "Can't bind onClickListener for reading News");
+                                    }
+
+                                }
+
+                            });
+
+                        }
+                    }
+
+                });
+            } else {
+
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        LinearLayout placeHolder = new LinearLayout(view.findViewById(R.id.scrollViewDashboard).getContext());
+                        inflater.inflate(R.layout.news_view, placeHolder);
+                        ((LinearLayout) view.findViewById(R.id.layoutDashboard)).addView(placeHolder);
+                        TextView txtNewsImgCaption = (TextView) view.findViewById(R.id.txtNewsImgCaption);
+                        ImageView imgNews = (ImageView) view.findViewById(R.id.imgNews);
+                        TextView txtNewsSubHeading = (TextView) view.findViewById(R.id.txtNewsSubheading);
+                        TextView txtNewsBody = (TextView) view.findViewById(R.id.txtNewsBody);
+                        txtNewsImgCaption.setVisibility(View.GONE);
+                        imgNews.setVisibility(View.GONE);
+                        txtNewsSubHeading.setText("Ooops!");
+                        txtNewsBody.setText("Leider konnten keine Nachrichten heruntergeladen werden.");
+                    }
+                });
+
+            }
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static Story getmSelectedStory() {
+        return mSelectedStory;
+    }
+
+    public static void setmSelectedStory(Story mSelectedStory) {
+        NewsHandler.mSelectedStory = mSelectedStory;
+    }
+
+}
---
+++ app/src/main/java/de/ehealth/project/letitrip_beta/model/news/Media.java
@@ -0,0 +1,34 @@
+package de.ehealth.project.letitrip_beta.model.news;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("org.jsonschema2pojo")
+public class Media {
+
+    @SerializedName("image")
+    @Expose
+    private List<Image> image = new ArrayList<Image>();
+
+    /**
+     *
+     * @return
+     * The image
+     */
+    public List<Image> getImage() {
+        return image;
+    }
+
+    /**
+     *
+     * @param image
+     * The image
+     */
+    public void setImage(List<Image> image) {
+        this.image = image;
+    }
+
+}

9e8ce407f6099ed8c64a96c64b208034c02094ac
LarsM1 <lars.melcher@freenet.de>
2016-02-24 17:19:30 +0100

    watt min is 0 now

--- app/src/main/java/de/ehealth/project/letitrip_beta/view/fragment/Session.java
+++ app/src/main/java/de/ehealth/project/letitrip_beta/view/fragment/Session.java
@@ -234,7 +234,7 @@ public class Session extends Fragment {
                 "humidity"+ ((float)humidity)/100+"\n"+
                 "watt: "+watt);
 
-        txtwatt.setText((temperature==-300?"N/A":df.format(watt)));
+        txtwatt.setText((temperature==-300?"N/A":(watt < 0?"0":df.format(watt))));
     }
 
     @Override
